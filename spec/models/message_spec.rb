9aabb0e43cc73114d7c8cd321accb327deab7993
Jake Brukhman <jbrukh@gmail.com>
2014-08-12 12:07:23 -0400

    Checking content length.

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -16,9 +16,17 @@ RSpec.describe Message, :type => :model do
 		}.to change(Message, :count).by(1)
 	end
 
-	it "has non-trivial content" do
-		m = Message.new(@valid_attributes.except(:content))
-		expect(m).not_to be_valid
+	describe "Content" do
+		it "is required" do
+			m = Message.new(@valid_attributes.except(:content))
+			expect(m).not_to be_valid
+			expect(m.errors[:content]).not_to be_nil
+		end
+		it "is at least 5 characters long" do
+			m = Message.new(:user_id => 1, :recipient_id => 2, :content => 'abc')
+			expect(m).not_to be_valid
+			expect(m.errors[:content]).not_to be_nil
+		end
 	end
 
 	describe "Associations" do
