892f6b1f725141a5fecb5b96770844e12e77adef
Jake Brukhman <jbrukh@gmail.com>
2014-08-12 12:11:36 -0400

    Added foreign key for recipient to model.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ActiveRecord::Base
 	belongs_to :user
-	belongs_to :recipient
+	belongs_to :recipient, class_name: 'User', foreign_key: 'recipient_id'
 	validates :content, presence: true, length: {minimum: 5}
 end
