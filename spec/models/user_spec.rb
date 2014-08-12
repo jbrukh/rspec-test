4e965c742b7de223d7d5263eb8052447be65e694
Jake Brukhman <jbrukh@gmail.com>
2014-08-12 13:27:51 -0400

    Streamlined user test.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,13 +6,25 @@ describe User, :type => :model do
 		expect(build(:user)).to be_valid
 	end
 
-	describe "Attributes" do
-		it "is invalid without required attributes" do
-			%w(name).each do |attr|
-				u = build(:user, attr => nil)
-				expect(u).not_to be_valid
-				expect(u.errors[attr]).not_to be_nil
-			end
+	it "is invalid when missing requited attributes" do
+		%w(name).each do |attr|
+			assert_invalid_user(attr, nil)
 		end
 	end
+
+	describe "name" do
+		it "is invalid when smaller than 3 characters" do
+			assert_invalid_user(:name, 'aa')
+		end
+
+		it "is invalid when larger than 16 characters" do
+			assert_invalid_user(:name, '12345678901234567')
+		end
+	end
+end
+
+def assert_invalid_user(attr, attr_value)
+	u = build(:user, attr => attr_value)
+	expect(u).not_to be_valid
+	expect(u.errors[attr]).not_to be_nil
 end
